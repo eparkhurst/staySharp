de190b3aeb698b7ae935c6fee10debcdb27614f7
eparkhurst <eli.parkhurst@gmail.com>
2016-05-18 14:23:41 -0600

    enabled cors and sent array of tea objects from backend. There isn't any sort of database at the moment

--- frontend/app/js/services.js
+++ frontend/app/js/services.js
@@ -1,3 +1,16 @@
 app.service('cartService', function(){
     this.cart = []
 })
+
+app.factory('batch_service', function($http){
+  return {
+    getBatches:function(){
+      return $http.get(config.host +'dashboard').then(function(data){
+        return data.data;
+      });
+    },
+    createBrew:function(brew){
+      return $http.post(config.host +'dashboard',brew);
+    }
+  };
+});
