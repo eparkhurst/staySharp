a40861a4ae2f9c75b8cfcda3cc3a6107fe910ec7
eparkhurst <eli.parkhurst@gmail.com>
2016-05-18 16:10:58 -0600

    disabled checkout button if no items in cart

--- frontend/app/js/controllers.js
+++ frontend/app/js/controllers.js
@@ -1,20 +1,32 @@
 app.controller("MainController", function($scope, $http, cartService){
-  $scope.line="this works"
+
   $scope.cart=[]
   $scope.numberOfItems = 0
+  $scope.checkoutLink = "#/"
+
+  $scope.checkout = function(){
+    if($scope.numberOfItems>0) $scope.checkoutLink = "#/checkout"
+  }
+
   $scope.setCategory=function(category){
     console.log(category);
   }
+
   $scope.addToCart = function(quantity, tea){
+    if(!quantity) return
     cartService.cart.push({tea:tea,quantity:quantity});
     console.log(cartService.cart);
     $scope.numberOfItems += parseInt(quantity);
   }
 
-  $http.get('http://localhost:3000/').then(function(data){
-      $scope.data = data.data;
+  $http.get('http://localhost:3000/').then(function successCallback(response){
+      $scope.data = response.data;
+  }, function errorCallback(response){
+    console.log(response);
   });
+
 })
+
 
 app.controller("checkoutController", function($scope, cartService){
   $scope.cart = cartService.cart;
